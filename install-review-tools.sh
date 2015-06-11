3c962f321b368f0f282269f1dfd5e39675e94715
Cory Johns <johnsca@gmail.com>
2015-06-11 14:26:38 -0400

    Added bundletester dependencies so they do not need to be installed during run

--- install-review-tools.sh
+++ install-review-tools.sh
@@ -3,7 +3,7 @@ set -e
 HOME=/home/ubuntu
 
 sudo apt-get update -qqy
-sudo apt-get install unzip -qy
+sudo apt-get install -qy unzip build-essential charm-tools python-dev python-pip python-virtualenv rsync
 sudo pip install bundletester flake8
 
 chown -R ubuntu:ubuntu ${HOME}
