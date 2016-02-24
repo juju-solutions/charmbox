e8fda8752b560399cb3642c052b7b94645e75155
Charles Butler <chuck@dasroot.net>
2016-02-24 17:34:06 -0500

    Juju 2.0 bump
    Trim the fat, restructure charmbox just a bit to make it more
    maintainable
    
    base off of the jujusolutions/jujubox:devel flavor for now.
    
    Confirmed 2.0 charmbox support - but tooling mileage may vary (i'm
    talking about you bundletester)

--- install-review-tools.sh
+++ install-review-tools.sh
@@ -2,8 +2,21 @@
 set -e
 HOME=/home/ubuntu
 
+sudo add-apt-repository ppa:yellow/ppa
 sudo apt-get update -qqy
-sudo apt-get install -qy unzip build-essential charm-tools python-dev python-pip python-virtualenv rsync
+sudo apt-get install -qy \
+                        build-essential \
+                        charm \
+                        charm-tools \
+                        python-dev \
+                        python-pip \
+                        python-virtualenv \
+                        rsync \
+                        unzip
+
 sudo pip install bundletester flake8 pyyaml --upgrade
+
+echo "export LAYER_PATH=${HOME}/layers" >> /home/ubuntu/.bashrc
+echo "export INTERFACE_PATH=${HOME}/interfaces" >> /home/ubuntu/.bashrc
 
 chown -R ubuntu:ubuntu ${HOME}
--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-## commented out for now until further work on lxc
-#/patchcontainer.sh
-#brctl addbr lxcbr0
-#ifconfig lxcbr0 10.0.4.1 up
-
 export HOME=/home/ubuntu
 cd $HOME
-chown ubuntu:ubuntu $HOME/.juju
+chown ubuntu:ubuntu $HOME/.local/share/juju
+cp $HOME/.bashrc $HOME/.bash_profile
 sudo -u ubuntu ssh-agent /bin/bash
