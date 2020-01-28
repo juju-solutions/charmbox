11202fbc4c3c8c9ed6fd6257936e35ea9270ce2e
Kevin W Monroe <kevin.monroe@canonical.com>
2020-01-28 11:56:58 -0600

    no charm apt pkg in bionic, build/install with go

--- charmbox-setup.sh
+++ charmbox-setup.sh
@@ -21,12 +21,32 @@ sudo apt-get install -qy  \
                      rsync  \
                      unzip
 
-sudo apt install --no-install-recommends charm
-
 sudo pip install --upgrade pip six
 sudo pip install amulet flake8 bundletester tox
 sudo pip3 install --upgrade pip
 sudo pip3 install amulet
+
+# charmstore-client should be installed with snap, but docker can't do that.
+# Live a little; let's GO!
+mkdir -p /tmp/charm-go /tmp/charm-gopath
+(
+    cd /tmp/charm-go
+    wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
+    tar zxf go1.12.6.linux-amd64.tar.gz
+)
+export GOROOT=/tmp/charm-go/go
+export GOPATH=/tmp/charm-gopath
+export PATH=${GOPATH}/bin:${GOROOT}/bin:${PATH}
+
+# NB: no go files in root, but we just need the source, so || true
+go get github.com/juju/charmstore-client || true
+(
+    cd ${GOPATH}/src/github.com/juju/charmstore-client
+    make deps install
+    sudo mv ${GOPATH}/bin/charm* /usr/local/bin
+)
+# kthxbye
+rm -rf /tmp/charm-go /tmp/charm-gopath
 
 # Install charm-tools from source
 git clone https://github.com/juju/charm-tools /tmp/charm-tools
