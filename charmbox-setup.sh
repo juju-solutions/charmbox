62eb4ec2b16599890fcbe25bfbb08f967634aaad
Kevin W Monroe <kevin.monroe@canonical.com>
2020-01-28 13:19:43 -0600

    remove matrix (no longer maintained)

--- charmbox-setup.sh
+++ charmbox-setup.sh
@@ -55,8 +55,3 @@ cd /tmp/charm-tools
 sudo pip3 install .
 cd ..
 rm -rf charm-tools
-git clone https://github.com/juju-solutions/matrix.git
-cd matrix
-sudo pip3 install . -f wheelhouse --no-index
-cd ..
-rm -rf matrix
